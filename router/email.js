aa8f469135171c96eed578bd2380a0c347cfcc1f
yaochaohang <yaochaohang219@gmail.com>
2023-07-27 14:42:42 +0800

    修改接口名称

--- router/email.js
+++ router/email.js
@@ -3,7 +3,8 @@ const home = new Router();
 const sender = require('../utils/sender')
 const validator = require('../utils/validator');
 
-home.post('/', async (ctx, next) => {
+home.post('/email', async (ctx, next) => {
+    console.log('调用接口')
     await next();
     const params = ctx.request.body
     if(validator(ctx, params)){
--- router/index.js
+++ router/index.js
@@ -2,7 +2,7 @@ const Router = require('koa-router');
 const router = new Router();
 const email = require('./email.js');
 
-router.use('/email', email.routes(), email.allowedMethods());
-router.redirect('/', '/email');
+router.use('/api', email.routes(), email.allowedMethods());
+router.redirect('/', '/api/email');
 
 module.exports = router;
